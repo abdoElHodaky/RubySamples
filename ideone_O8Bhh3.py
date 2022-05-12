18e362e08c5b84afbbec297488de1b9f4fe448f5
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-05-12 18:27:03 +0200

    Add files via upload

---
+++ ideone_O8Bhh3.py
@@ -0,0 +1,17 @@
+# your code goes here
+'''arr=[1,2,1,2]
+res=[]
+counted=[]
+for i in arr:
+ if arr.count(i)>1:
+  counted.append(i)
+ else:
+  res.append(i)
+_counted=[]
+for i in counted:
+ if i not in _counted:
+  _counted.append(i)
+print(res)'''
+
+s=["ad","4"]
+print(int("ad")+int("4"))
